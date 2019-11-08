0940a53dbc9aeeb7da05f9399540deb54a205fc0
Lucas Sales <lucassales398@gmail.com>
2019-11-07 22:53:50 -0300

    finalizei o crud-host

--- host-crud/home.php
+++ host-crud/home.php
@@ -2,6 +2,10 @@
     session_start();
     if(!isset($_SESSION['auth']) || $_SESSION['auth'] === false)
         header('Location: index.html');
+
+    require_once('hostcrud.php');
+    $crud = new HostCrud();
+    $hosts = $crud->readAll();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,40 +14,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Home</h1>
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-adicionar-tab" data-toggle="tab" href="#nav-adicionar" role="tab" aria-controls="nav-adicionar" aria-selected="true">Adicionar</a>
+            <a class="nav-item nav-link" id="nav-alterar-tab" data-toggle="tab" href="#nav-alterar" role="tab" aria-controls="nav-alterar" aria-selected="false">Alterar</a>
+            <a class="nav-item nav-link" id="nav-deletar-tab" data-toggle="tab" href="#nav-deletar" role="tab" aria-controls="nav-deletar" aria-selected="false">Deletar</a>
+            <a class="nav-item nav-link" id="nav-ver-tab" data-toggle="tab" href="#nav-ver" role="tab" aria-controls="nav-ver" aria-selected="false">Ver Todos</a>
+        </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-adicionar" role="tabpanel" aria-labelledby="nav-adicionar-tab">
+            <h2>Adicionar</h2>
+            <form action="create.php" method="post">
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" placeholder="nome">
+                <label for="address">Address:</label>
+                <input type="text" name="address" placeholder="address">
+                <input class="btn btn-outline-secondary" class="btn btn-outline-secondary" type="submit" value="Adicionar">
+            </form>
+        </div>
+        <div class="tab-pane fade" id="nav-alterar" role="tabpanel" aria-labelledby="nav-alterar-tab">
+            <h2>Alterar</h2>
+            <form action="update.php" method="post">
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" placeholder="nome">
+                <label for="address">Address:</label>
+                <input type="text" name="address" placeholder="address">
+                <label for="id">id:</label>
+                <input type="text" name="id" placeholder="id">
+                <input class="btn btn-outline-secondary" type="submit" value="Alterar">
+            </form>
+        </div>
+        <div class="tab-pane fade" id="nav-deletar" role="tabpanel" aria-labelledby="nav-deletar-tab">
+            <h2>Deletar</h2>
+            <form action="delete.php" method="post">
+                <label for="id">id:</label>
+                <input type="text" name="id" placeholder="id">
+                <input class="btn btn-outline-secondary" type="submit" value="Deletar">
+            </form>
+        </div>
+        <div class="tab-pane fade" id="nav-ver" role="tabpanel" aria-labelledby="nav-ver-tab">
+            <?php if (sizeof($hosts) != 0): ?>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($hosts as $host): ?>
+                            <tr>
+                                <th scope="row"><?php echo $host['id'] ?></th>
+                                <td><?php echo $host['name'] ?></td>
+                                <td><?php echo $host['address'] ?></td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            <?php else: ?>
+                <h2>Lista vazia</h2>
+            <?php endif ?>
+        </div>
+    </div>
 
-    <h2>Adicionar</h2>
-    <form action="create.php" method="post">
-        <label for="nome">Nome:</label><br>
-        <input type="text" name="nome" placeholder="nome"><br>
-        <label for="address">Address:</label><br>
-        <input type="text" name="address" placeholder="address"><br>
-        <input type="submit" value="Adicionar">
-    </form>
-
-    <h2>Alterar</h2>
-    <form action="update.php" method="post">
-        <label for="nome">Nome:</label><br>
-        <input type="text" name="nome" placeholder="nome"><br>
-        <label for="address">Address:</label><br>
-        <input type="text" name="address" placeholder="address"><br>
-        <label for="id">id:</label><br>
-        <input type="text" name="id" placeholder="id"><br>
-        <input type="submit" value="Alterar">
-    </form>
-
-    <h2>Deletar</h2>
-    <form action="delete.php" method="post">
-        <label for="id">id:</label><br>
-        <input type="text" name="id" placeholder="id">
-        <input type="submit" value="Deletar">
-    </form>
-    <a href="read.php" class="btn btn-outline-secondary">Ver todos</a><br>
-    <a href="logout.php" class="btn btn-primary">logout</a>
+    
+    <a href="logout.php" class="sair btn btn-primary">Sair</a>
 
 
 
